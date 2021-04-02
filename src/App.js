c9781df5dcf17ea3120b72ead2867e49149034b8
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-01 20:50:37 -0700

    config firebase

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import firebase from 'firebase'
 
 class App extends Component {
+  componentWillMount () {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyC3auvPSYOZOFZd5fR63--X7yV6ewsDDT8',
+      authDomain: 'manager-caa99.firebaseapp.com',
+      projectId: 'manager-caa99',
+      storageBucket: 'manager-caa99.appspot.com',
+      messagingSenderId: '768408312701',
+      appId: '1:768408312701:web:1ccb89b1e0c29b5ac38a14',
+      measurementId: 'G-7D9ELZTZLK'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
   render() {
     return (
       <Provider store={createStore()}>

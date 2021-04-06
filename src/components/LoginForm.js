82225189950c260f433a62a0716b083456eba570
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-05 20:55:53 -0700

    imported common components and rendered input for email

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { Card } from './common'
+import { Card, CardSection, Input, Button } from './common'
 
 class LoginForm extends Component {
   render() {
     return (
-      <Card></Card>
+      <Card>
+        <CardSection>
+          <Input 
+            label="Email"
+            placeholder="email@gmail.com"
+          />
+        </CardSection>
+      </Card>
     )
   }
 }

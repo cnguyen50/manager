e76ca56eb5f549c0befc82ed74047a22ed8c8672
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-08 21:56:48 -0700

    set new case and returned new state obj

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,6 +3,9 @@ const INITIAL_STATE = { email: ''}
 
 export default (state = INITIAL_STATE, actions) => {
   switch (actions.type) {
+    case EMAIL_CHANGED:
+      state.email = action.payload
+      return { ...state, email: action.payload }
     default:
       return state;
   }

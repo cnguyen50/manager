e16178ddf4156a197307b6a1b83c26dc85a680c9
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-12 22:41:29 -0700

    made the store prop more legible

--- src/App.js
+++ src/App.js
@@ -20,8 +20,10 @@ class App extends Component {
     firebase.initializeApp(firebaseConfig);
   }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     )

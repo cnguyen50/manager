5c953d96921f83c4471ac2d01b77d753d30b88a5
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-12 22:38:15 -0700

    imported applyMiddleware from redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import firebase from 'firebase'

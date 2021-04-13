26e161e905b11dfc54510805ecf9038cf778d78c
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-12 22:37:49 -0700

    imported ReduxThunk into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import firebase from 'firebase'
 import LoginForm from './components/LoginForm'

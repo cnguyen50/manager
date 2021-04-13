c8a9126d0c5e7339f5713c0a34eafb0990d9acc0
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-12 22:40:18 -0700

    wired up middleware with ReduxThunk as params

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   }
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <LoginForm />
       </Provider>
     )

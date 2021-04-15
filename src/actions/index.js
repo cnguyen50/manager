7d5c9320d05fedf0190557689677a6e9ee656a5f
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-14 22:18:14 -0700

    error handled

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,7 +26,8 @@ export const loginUser = ({ email, password }) => {
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() =>{
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => loginUserSuccess(dispatch, user))
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch))
       })
   }
 }

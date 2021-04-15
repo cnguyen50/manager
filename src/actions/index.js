ad2ee5037aaba0401e4c2ac49ce7ac784c3e22de
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-14 22:14:03 -0700

    revised loginUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,14 +18,10 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
-      })
+      .then(user => loginUserSuccess(dispatch, user))
       .catch(() =>{
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
-        })
+        .then(user => loginUserSuccess(dispatch, user))
       })
   }
 }

8e73cfbb9423c0696782d6166708a8551c332dc5
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-15 21:24:08 -0700

    renderError method

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,6 +18,18 @@ class LoginForm extends Component {
     this.props.loginUser({ email, password })
   }
 
+  renderError () {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white'}}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      )
+    }
+  }
+
   render() {
     return (
       <Card>

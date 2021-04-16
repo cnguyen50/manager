b7b3a5305676e6e6f1a4afc206fe1ce895b608ca
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-15 21:13:53 -0700

    imported login user fail

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types'
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types'
 
 const INITIAL_STATE = { 
   email: '', 

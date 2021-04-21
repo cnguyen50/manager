8bb49996e605eb340139442360d3a121ebfd0d38
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-20 22:44:25 -0700

    hideNavBar to root scene to remove padding

--- src/Router.js
+++ src/Router.js
@@ -6,12 +6,12 @@ import EmployeeList from './components/EmployeeList'
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 75 }}>
-      <Scene key ="root">
+      <Scene key ="root" hideNavBar>
         <Scene key="auth">
           <Scene key="login" component={LoginForm} title="Please Login"/>
         </Scene>
         <Scene key="main">
-          <Scene key ="employeeList" component={EmployeeList} title="Employees" initial /> 
+          <Scene key ="employeeList" component={EmployeeList} title="Employees" initial />
         </Scene> 
       </Scene>
     </Router>

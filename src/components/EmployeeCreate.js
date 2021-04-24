89183ba3c92ef75a036153c43c5b98409290e415
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-23 21:04:25 -0700

    imported connect from react redux

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import { React, Component } from 'react'
+import { connect } from 'react-redux'
 import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {

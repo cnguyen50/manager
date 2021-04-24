c030fc623bc2dfef7a05a9e50a2ed8462cdecd2f
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-23 21:01:57 -0700

    imported employeeFromReducer into reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import AuthReducer from './AuthReducer'
+import EmployeeFormReducer from './EmployeeFormReducer'
 
 export default combineReducers({
   auth: AuthReducer

20c391b6ee89c2fb768121b5593cb648023fc5e4
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-23 21:07:24 -0700

    made mapStateToProps function

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -31,4 +31,9 @@ class EmployeeCreate extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
 export default connect(null, { employeeUpdate })(EmployeeCreate)

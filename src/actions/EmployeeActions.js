13883decd16734cc81f6bf3ff8f4ea2bdeea5b51
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-28 22:32:43 -0700

    imported router flux

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 import {
   EMPLOYEE_UPDATE
 } from './types'
+import { Actions } from 'react-native-router-flux'
 
 export const employeeUpdate = ({ prop, value }) => {
   return {

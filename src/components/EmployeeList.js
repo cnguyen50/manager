8e4f6d5a7b91f041611aceb3bcfbbfad6ee7b9f8
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-04-30 05:51:05 -0700

    mount to get employees

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -4,6 +4,10 @@ import { View, Text } from 'react-native'
 import { employeeFetch } from '../actions'
 
 class EmployeeList extends Component {
+  componentDidMount() {
+    this.props.employeeFetch()
+  }
+  
   render() {
     return (
       <View>

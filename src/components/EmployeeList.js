71ea481d8931643c767a5ea5aa9fbe1753182500
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-05-03 02:51:12 -0700

    imported ListView

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text } from 'react-native'
+import { View, Text, ListView } from 'react-native'
 import { employeeFetch } from '../actions'
 
 class EmployeeList extends Component {
   componentDidMount() {
     this.props.employeeFetch()
   }
+
+
   
   render() {
     return (

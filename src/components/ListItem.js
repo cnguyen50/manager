de21689226ec4dbcd0a582242136c45d11c5ffe9
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-05-04 23:12:47 -0700

    created boilerplate for ListItem

---
+++ src/components/ListItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { CardSection } from './common'
+
+class ListItem extends Component {
+  render() {
+    <Text></Text>
+  }
+}

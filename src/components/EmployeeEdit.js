55cbf92854e0b6f6a97680f31379b89eff2f93f9
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-05-07 22:45:56 -0700

    EmployeeEdit boilerplate

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Card, CardSection, Button } from './common'
+
+class EmployeeEdit extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Button>
+            Save
+          </Button>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+export default EmployeeEdit
